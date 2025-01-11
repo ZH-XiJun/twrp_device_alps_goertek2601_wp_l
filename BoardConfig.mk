df1766d8220a9f6b5db33899fe615610a475066b
ZH-XiJun <59949135+ZH-XiJun@users.noreply.github.com>
2025-01-11 13:20:18 +0800

    use watch-friendly theme

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_hdpi
+TW_THEME := watch_mdpi
 TW_EXTRA_LANGUAGES := false
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
